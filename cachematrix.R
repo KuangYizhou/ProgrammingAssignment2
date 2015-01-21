453f8848b88d0c7127f930b79d9185d46263abdf
KuangYizhou <461351800@qq.com>
2015-01-21 10:40:15 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -23,20 +23,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function (x, ...) {
   # Need to compare matrix to what was there before!
   m <- x$getinverse() # if an inverse has already been calculated this gets it
   if(!is.null(m))# check to see if cacheSolve has been run before
     { 
-    message("getting cached data")# check that matrix hasn't changed,if hasn't, sends a text message and returns the cached matrix
-      #parts removed
+    message("getting cached data")# check that matrix hasn't changed, and if it hasn't, sends a text message and returns the cached matrix
       return(m)
     }
     # otherwise 
-    y <- x$getmatrix() # get the value of the input matrix
-    x$setmatrix(y) # run the setmatrix function to cache it
-    m <- solve(y, ...)
-    x$setinverse(m) # cache the inverse
+    y <- x$getmatrix() # run the getmatrix function to get the value of the input matrix
+    x$setmatrix(y) # run the setmatrix function on the input matrix to cache it
+    m <- solve(y, ...) # compute the value of the inverse of the input matrix
+    x$setinverse(m) # run the setinverse function on the inverse to cache the inverse
     m # return the inverse
   }
